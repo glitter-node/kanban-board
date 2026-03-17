d380429b5d3d530cd11adc1b0676b3fac9d8a698
glitter gim <gim@glitter.kr>
2026-03-18 02:26:41 +0900

    fixing a runtime error

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,10 @@ import forms from '@tailwindcss/forms';
 export default {
     darkMode: 'class',
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+        './app/**/*.php',
     ],
 
     theme: {
