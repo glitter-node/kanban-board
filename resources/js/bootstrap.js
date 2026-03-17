8ffb03943ddea1c767a2e8e09d3cdb52336f5a03
glitter gim <gim@glitter.kr>
2026-03-17 16:48:26 +0900

    Update endpoint: wss://reverb-ws.glitter.tw

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,19 +6,13 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
-const pusher = new Pusher('xi8cgvcxkjpn6r3rrxir', {
+window.Pusher = Pusher;
+window.Echo = new Echo({
+    broadcaster: 'reverb',
+    key: 'xi8cgvcxkjpn6r3rrxir',
     wsHost: 'reverb-ws.glitter.tw',
-    wssHost: 'reverb-ws.glitter.tw',
     wsPort: 443,
     wssPort: 443,
     forceTLS: true,
-    enabledTransports: ['ws'],
-    disableStats: true,
-});
-
-window.Pusher = Pusher;
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: 'xi8cgvcxkjpn6r3rrxir',
-    client: pusher,
+    enabledTransports: ['ws', 'wss'],
 });
